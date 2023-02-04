b80f5833ccba307a80d35e2f116167a5cf9747d0
Trey <104473511+Lemonns@users.noreply.github.com>
2023-02-03 18:26:22 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+app.get("/contact-me", (req, res) => {
+    res.render("contact-me");
+});
+
+app.use((req, res, next) => {
+    res.status(404).send(res.render("404"));
+});
+
+app.listen(port, function () {
+    console.log(`App listening on port ${port}!`)
+});
